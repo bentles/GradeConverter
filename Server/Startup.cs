636d71ce48bbdf185845aa6ebc16f570e1b44d9e
bentles <dee.bent.lee@gmail.com>
2020-12-25 08:09:22 +0200

    testing on live because nobody is using it lol

--- Server/Startup.cs
+++ Server/Startup.cs
@@ -37,9 +37,12 @@ namespace GradeConverter.Server
             }
             else
             {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+
+                app.UseDeveloperExceptionPage();
+                app.UseWebAssemblyDebugging();
+                // app.UseExceptionHandler("/Error");
+                // // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                // app.UseHsts();
             }
 
             app.UseHttpsRedirection();
