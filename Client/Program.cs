014b276e58904c21c5bee531fdcbb7099d945761
bentles <dee.bent.lee@gmail.com>
2020-12-25 12:01:41 +0200

    added mudblazor and tried and failed to make things prettier using it

--- Client/Program.cs
+++ Client/Program.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using MudBlazor;
+using MudBlazor.Services;
 
 namespace GradeConverter.Client
 {
@@ -18,7 +20,9 @@ namespace GradeConverter.Client
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+            builder.Services.AddMudBlazorDialog();
+            builder.Services.AddMudBlazorSnackbar();
+            builder.Services.AddMudBlazorResizeListener();
             await builder.Build().RunAsync();
         }
     }
