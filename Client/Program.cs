3cccd171e8c23285357f0fd59eb8ed644b2ec2fe
bentles <dee.bent.lee@gmail.com>
2020-12-25 13:04:01 +0200

    slightly nicer UI

--- Client/Program.cs
+++ Client/Program.cs
@@ -20,9 +20,11 @@ namespace GradeConverter.Client
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            
             builder.Services.AddMudBlazorDialog();
             builder.Services.AddMudBlazorSnackbar();
             builder.Services.AddMudBlazorResizeListener();
+
             await builder.Build().RunAsync();
         }
     }
